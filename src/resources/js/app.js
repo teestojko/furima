3509f719e87562e8d1b7accd71ed36fa5cc4d976
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-14 01:24:54 +0900

    ”ナビアニメーション”

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -18,21 +18,8 @@ require('./components/Example');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sidebar from './components/sidebar';
-import { createApp } from 'vue';  // Vue 3 のインポート
-import IndexNav from './components/IndexNav.vue';  // 作成したコンポーネントのインポート
-import IndexForm from './components/IndexForm.vue';    // 検索フォームコンポーネント
 
 if (document.getElementById('sidebar')) {
     ReactDOM.render(<Sidebar />, document.getElementById('sidebar'));
 }
-
-// Vue アプリケーションを作成
-const app = createApp({});
-
-// コンポーネントをグローバルに登録
-app.component('index_nav', IndexNav);
-app.component('index_form', IndexForm);
-
-// Vue アプリケーションをマウント
-app.mount('#app');
 
