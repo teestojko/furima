5092cec14c45b3160e84c5e839062ee6fdb066ba
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-15 21:24:45 +0900

    ”react検索アニメーション”

--- src/resources/js/components/sidebar.jsx
+++ src/resources/js/components/sidebar.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import '../styles/sidebar.css'; // CSSファイルのパス
+import React, { useState } from "react";
+import "../styles/sidebar.css"; // CSSファイル
 
 const Sidebar = () => {
     const [isHovered, setIsHovered] = useState(false);
+    const userId = 1; // 適切なユーザーIDを設定
 
     return (
         <div
@@ -10,16 +11,51 @@ const Sidebar = () => {
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
         >
-        <div className={`sidebar ${isHovered ? 'visible' : 'hidden'}`}>
-            <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            </ul>
+        <div className={`sidebar ${isHovered ? "visible" : "hidden"}`}>
+            <div className="index_nav">
+            <div className="index_products_create">
+                <a
+                className={`products_create_link ${isHovered ? "hovered" : ""}`}
+                href="/products/create"
+                >
+                出品
+                </a>
+            </div>
+            <div className="index_profile">
+                <a
+                className={`profile_link ${isHovered ? "hovered" : ""}`}
+                href={`/profile/${userId}`}
+                >
+                プロフィール
+                </a>
+            </div>
+            <div className="index_cart">
+                <a
+                className={`cart_link ${isHovered ? "hovered" : ""}`}
+                href="/cart"
+                >
+                カート
+                </a>
+            </div>
+            <div className="index_coupon">
+                <a
+                className={`coupon_link ${isHovered ? "hovered" : ""}`}
+                href="/coupons"
+                >
+                クーポン一覧
+                </a>
+            </div>
+            <div className="index_my_page">
+                <a
+                className={`my_page_link ${isHovered ? "hovered" : ""}`}
+                href="/my_page"
+                >
+                マイページ
+                </a>
+            </div>
+            </div>
         </div>
-        <div className="arrow">
-            &larr; {/* 左矢印 */}
-        </div>
+        <div className="arrow">&larr; {/* 左矢印 */}</div>
         </div>
     );
 };
