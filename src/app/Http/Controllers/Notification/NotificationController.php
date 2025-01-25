592641dfd258cc477a53ed12f85d574921a7f7e5
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-25 22:34:14 +0900

    ”レポート機能削除”

--- src/app/Http/Controllers/Notification/NotificationController.php
+++ src/app/Http/Controllers/Notification/NotificationController.php
@@ -12,24 +12,38 @@ class NotificationController extends Controller
 {
     public function showNotification()
     {
+        // 現在の認証ユーザーのIDを取得
         $userId = Auth::id();
-        // メッセージ履歴
+
+        // メッセージ履歴を取得
+        // 受信者または送信者が現在のユーザーである場合のメッセージを取得
         $messages = Message::where('receiver_id', $userId)
             ->orWhere('sender_id', $userId)
+            // 送信者および受信者の情報も一緒に取得
             ->with('sender', 'receiver')
+            // 最新のメッセージを先頭に取得
             ->latest()
             ->get();
+
         $notifications = Notification::where('user_id', Auth::id())
-        ->orderBy('created_at', 'desc')
-        ->get();
+            ->orderBy('created_at', 'desc')
+            ->get();
+        // dd($notifications);
+
         $unreadNotifications = $notifications->where('is_read', false);
+        // dd($unreadNotifications);
+
         $readNotifications = $notifications->where('is_read', true);
-        return view('notification.show', [
-            'unreadNotifications' => $unreadNotifications,
-            'readNotifications' => $readNotifications,
-        ]);
-        return view('notification.show', compact('messages', 'notifications'));
+        // dd($readNotifications);
+
+        // return view('notification.show', [
+        //     'unreadNotifications' => $unreadNotifications,
+        //     'readNotifications' => $readNotifications,
+        // ]);
+
+        return view('notification.show', compact('messages', 'notifications', 'unreadNotifications', 'readNotifications' ));
     }
+
 
     public function markAsReadAndMessageDetail($notificationId)
     {
