751b5ba71303e5dd0a0a0d8d8fd765f6a80746ac
teestojko <158604040+teestojko@users.noreply.github.com>
2025-02-24 15:00:28 +0900

    ”コンポーネントをアッパーキャメルに変更”

--- src/resources/js/app.tsx
+++ src/resources/js/app.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client'; // React 18 では `react-dom/client` を使う
-import SearchForm from './components/searchForm';
-import Sidebar from './components/sidebar';
+import SearchForm from './components/SearchForm';
+import Sidebar from './components/Sidebar';
 import { randomizeLights } from './components/LightEffect';
 import FileDisplay from './components/FileDisplay';
 
