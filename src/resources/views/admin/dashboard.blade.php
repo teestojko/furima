4ce1ce5d8a453e220ec71e02be0e4090ceb55e40
teestojko <158604040+teestojko@users.noreply.github.com>
2025-04-10 20:41:14 +0900

    ”サイドバーをアプリ内のみに適用”

--- src/resources/views/admin/dashboard.blade.php
+++ src/resources/views/admin/dashboard.blade.php
@@ -1,32 +1,33 @@
-@extends('layout.app')
-
-@section('css')
-<link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
-<link rel="stylesheet" href="{{ asset('css/admin/dashboard.css') }}">
-@endsection
-
-@section('content')
-<div class="dashboard">
-    <h1 class="dashboard_title">管理者ダッシュボード</h1>
-
-    <div class="dashboard_content">
-        <div class="dashboard_link">
-            <a href="{{ route('admin.coupons-create') }}" class="btn btn_primary">クーポン作成</a>
-        </div>
-
-        <div class="dashboard_nav">
-            <nav class="dashboard_nav_btn">
-                <a class="btn btn_primary" href="{{ route('admin.categories.index') }}">
-                    カテゴリー管理
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FURIMA</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/dashboard.css') }}">
+</head>
+<body>
+    <div class="dashboard">
+        <h1 class="dashboard_title">管理者ダッシュボード</h1>
+        <div class="dashboard_content">
+            <div class="dashboard_link">
+                <a href="{{ route('admin.coupons-create') }}" class="btn btn_primary">クーポン作成</a>
+            </div>
+            <div class="dashboard_nav">
+                <nav class="dashboard_nav_btn">
+                    <a class="btn btn_primary" href="{{ route('admin.categories.index') }}">
+                        カテゴリー管理
+                    </a>
+                </nav>
+            </div>
+            <div class="dashboard_link">
+                <a href="{{ route('admin.reports.index') }}" class="btn btn_primary">
+                    通報一覧を確認する
                 </a>
-            </nav>
+            </div>
         </div>
-        <div class="dashboard_link">
-            <a href="{{ route('admin.reports.index') }}" class="btn btn_primary">
-                通報一覧を確認する
-            </a>
-        </div>
-
     </div>
-</div>
-@endsection
+</body>
+</html>
