e1de4c0a93cb1e6c4aeb5432ca56729857ed0a20
teestojko <158604040+teestojko@users.noreply.github.com>
2025-06-05 23:17:33 +0900

    tailwind.css実装

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -3,6 +3,9 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.tsx', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ])
     .postCss('resources/js/styles/Sidebar.css', 'public/css')
     .options({
         processCssUrls: false,
@@ -14,4 +17,3 @@ mix.webpackConfig({
         extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
     }
 });
-
